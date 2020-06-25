e4bf0651a4b7174703b540a96fc2e730bfb026ab
Mahmoud Zakria <me@zakcoding.com>
2020-06-26 00:46:02 +0200

    fix the public path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/Loan-calculator/dist/" : ""
+  publicPath: process.env.NODE_ENV === "production" ? "/Gydio/dist/" : "",
 };
